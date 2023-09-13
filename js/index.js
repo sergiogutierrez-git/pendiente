0d022dfcaa563151e5e422ba1e21f5e4941cdbab
Sergio <sergio.desasoft@gmail.com>
2023-09-13 15:47:26 -0300

    arreglo dataAvanceX

--- js/index.js
+++ js/index.js
@@ -3,22 +3,32 @@ var btn = document.getElementById("btn")
 btn.addEventListener("click", function(){
     var avanceX = parseFloat(document.getElementById("x").value);
     var avanceY = parseFloat(document.getElementById("y").value);
+
+    var texto = document.getElementById("parrafo");
         
     var pendiente = avanceY / avanceX;
         
     var dataAvanceX = parseFloat(document.getElementById("xCalc").value);
     var dataAvanceY = parseFloat(document.getElementById("yCalc").value);
-    
-    if (dataAvanceX == null) {
-        var rAvanceX = dataAvanceY / pendiente;
-        var texto = document.getElementById("parrafo");
-        texto.innerText = "Cada " +rAvanceX+ " metros debe bajar " +dataAvanceY+ " metros.";
-    } else {
-        var rAvanceY = pendiente * dataAvanceX;
-        var texto = document.getElementById("parrafo");
-        texto.innerText = "Cada " +dataAvanceX+ " metros debe bajar " +rAvanceY+ " metros.";
-    }
 
+    if (document.getElementById("xCalc").value == "") {
+        faltaX(pendiente, dataAvanceY,texto);
+    } else {
+        faltaY(pendiente, dataAvanceX,texto);
+    }
+    
     var resultados = document.getElementById("mostrar");
     resultados.style = "display:block";
 })
+
+function faltaX (pendiente,y,texto) {
+    var datoY = y;
+    var datoX = y / pendiente;
+    texto.innerText = "Cada " +datoX+ " metros debe bajar " +datoY+ " metros.";
+}
+
+function faltaY (pendiente,x,texto) {
+    var datoX = x;
+    var datoY = pendiente * datoX;
+    texto.innerText = "Cada " +datoX+ " metros debe bajar " +datoY+ " metros.";
+}
